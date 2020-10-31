e918d33ea0b37dfcd5d77190cef3838434425ade
Temur <tim_1998@bk.ru>
2020-10-31 10:50:21 +0300

    Fix

--- mongodb/init-mongo.js
+++ mongodb/init-mongo.js
@@ -1,4 +1,4 @@
-var db = connect("mongodb://root:personalguide2020@localhost:27017/admin");
+var db = connect("mongodb://admin:personalguide2020@localhost:27017/admin");
 
 db = db.getSiblingDB('database'); // we can not use "use" statement here to switch db
 
