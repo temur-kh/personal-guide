5dfbd2ab0e72bebbd0b766bd383bcbdf853d02f0
Chernyshenko <chernyshenko.a@gmail.com>
2020-12-03 10:02:31 +0400

    Route alg (#13)
    * First version of TSP algoritm. It takes cafes in Berlin near center and calculates optimal route but with Euclidean distances
    
    * plots with leaflet
    
    * snake code style for project
    
    * routing model version 2
    
    * add class Optimizer
    
    * small optimizations
    
    * Добавил опцию, нужно ли возвращаться в стартовую точку
    
    * Обновил подсчет расстояний
    
    * Добавил возможность получить пути в солвер, вынес проверку на то что стартовая точка не из списка интересных
    
    * Добавил перебор эвристик, подсчет длины маршрута и количества точек. выбирается оптимальная
    
    * Update requirements.txt
    
    * обновил алгоритм для замкнутого и не замкнутого маршрута
    
    * Добавил проверку на то, что точки в графе совпадают
    
    * reward collecting tsp
    
    * Если не нужно возврашаться, расстояния до стартовой точки зануляются. Из route удаляется последняя стартовая точка
    
    * поправил функции ограничения маршрута
    
    * merge conflicts solved
    
    * OR-tools требует целые расстояния, добавил преобразование в int
    
    * thread pool for route solvers. Solver for clusters of points
    
    Co-authored-by: Temur <tim_1998@bk.ru>

--- backend/optimal_route/main.py
+++ backend/optimal_route/main.py
@@ -9,15 +9,24 @@ berlinCenter = (52.5198810, 13.4073380)
 
 
 def main():
-    time_for_route = 40  # minutes
+    time_for_route = 100  # minutes
     speed = 100  # meters in minute
     need_return = False
 
     data = dt.create_data_model_distance()
 
+    nclusters = 2
+    clusters = dt.create_clusters(data['nv'], nclusters)
+    print(clusters)
+
     opt = Optimizer(speed=speed)
-    route = opt.solve(data, time_for_route, need_return=need_return)
-    print(route)
+    #route = opt.solve(data, time_for_route, need_return=need_return)
+    #route = opt.solve_clusters(data, clusters, time_for_route, need_return=need_return)
+    routes = opt.solve_parallel(data, clusters, time_for_route, need_return)
+
+    print('final route:')
+    for route in routes:
+        print(route)
 
 
 if __name__ == '__main__':
